5a14293a83e3a65bd233eab4430926d20ad57fb6
litz-a <litz-a@glodon.com>
2018-08-30 09:31:54 +0800

    first commit

---
+++ spring-boot-security-oauth2-sso/sso-client2/src/main/java/cn/lee/soo/client/SsoClient2Application.java
@@ -0,0 +1,18 @@
+package cn.lee.soo.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+
+/**
+ * @author litz-a
+ */
+@EnableOAuth2Sso
+@SpringBootApplication
+public class SsoClient2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SsoClient2Application.class, args);
+  }
+}
---
+++ spring-boot-security-oauth2-sso/sso-server/src/main/java/cn/lee/sso/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package cn.lee.sso.service.impl;
+
+import cn.lee.sso.constant.Constant;
+import cn.lee.sso.service.UserService;
+import okhttp3.OkHttpClient;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author litz-a
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+  private OkHttpClient okClient = new OkHttpClient();
+
+  /**
+   * 账户认证
+   */
+  @Override
+  public String acountAuthenticate(String account, String password) throws Exception {
+    return Constant.PASS;
+  }
+}
